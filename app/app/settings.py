040e8d08960fb5aca7cc95774f9a8cee5ab4b713
sbcvoxx <sbcvox@gmail.com>
2024-11-17 22:09:57 +0200

    Implemented user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+
     'core',
 ]
 
@@ -130,3 +130,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
