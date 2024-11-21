4b258c9b0c10a103ec829d1deaebe3f459b9b03c
sbcvoxx <sbcvox@gmail.com>
2024-11-21 04:57:04 +0200

    Implemented tags api

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -1,14 +1,15 @@
 """
 URL mappings for the recipe app
 """
-from django.urls import path, include
+from django.urls import path, include # type: ignore
 
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter # type: ignore
 
 from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
 
 app_name = 'recipe'
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,11 @@
 """
 Views from the recipe APIs
 """
-from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, mixins # type: ignore
+from rest_framework.authentication import TokenAuthentication # type: ignore
+from rest_framework.permissions import IsAuthenticated # type: ignore
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 from recipe import serializers
 
 
@@ -31,3 +31,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """ Create a new recipe """
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
+    """ Manage tags in database """
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """ Filter queryset to authenticated user """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
