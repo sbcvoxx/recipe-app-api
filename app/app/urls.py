94b21d4606e6d4241cb56386afb439b747807745
sbcvoxx <sbcvox@gmail.com>
2024-11-26 03:31:48 +0200

    Added health check api

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,8 +22,11 @@ from django.urls import path, include  # type: ignore
 from django.conf.urls.static import static  # type: ignore
 from django.conf import settings  # type: ignore
 
+from core import views as core_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health-check', core_views.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render  # noqa
+"""
+Core views
+"""
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+def health_check(request):
+    """ Returns successful health check """
+    return Response({'healthy': True})
